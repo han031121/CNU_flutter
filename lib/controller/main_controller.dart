93c9b352a059761e2071a0a4aaa7897c3e82ccdb
han03 <han031121@gmail.com>
2024-08-24 11:24:03 +0900

    4주차 과제

--- lib/controller/main_controller.dart
+++ lib/controller/main_controller.dart
@@ -1,7 +1,21 @@
 import 'package:cnu_flutter/main.dart';
+import 'package:cnu_flutter/model/music_info.dart';
+import 'package:cnu_flutter/repository/music_info_repository.dart';
 import 'package:get/get.dart';
 
 class MainController extends GetxController{
+  final _repo = Get.find<MusicInfoRepository>();
+  final musicList = <MusicInfo>[].obs;
 
+  Future<void> getMusicList() async{
+    final res = await _repo.getMusicList();
+    res.fold((l) => print(l), (r) => musicList.addAll(r));
+  }
+
+  @override
+  void onInit() {
+    super.onInit();
+    getMusicList();
+  }
 }
 
